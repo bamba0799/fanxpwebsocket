2de009ab69fc0a2699217add699123a5b7a25c8d
Melone <andy.30yahoo.com>
2023-05-15 17:10:38 +0000

    feat-build-teams-service(#3)

--- src/routes/teams.ts
+++ src/routes/teams.ts
@@ -1,9 +1,17 @@
 import express from "express";
-import * as teamsRoutes from "../controllers/teams/fav";
-
+import * as favteamsRoutes from "../controllers/teams/fav";
+import * as teamsRoutes from "../controllers/teams/index";
 const router = express.Router();
 
-router.get("/fav/:userId", teamsRoutes.findAll);
+router.get("/fav/:userId", favteamsRoutes.findAll);
+
+
+// Teams
+router.get("/", teamsRoutes.getAll);
+router.get("/:teamId", teamsRoutes.getOne);
+router.post("/", teamsRoutes.post);
+router.delete("/:teamId", teamsRoutes.deleteTeam);
+router.put("/:teamId", teamsRoutes.updateTeam);
 // TODO: ...
 
 export default router;
