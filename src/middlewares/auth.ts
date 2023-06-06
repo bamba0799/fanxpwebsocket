0899842e6b222f739d9a1305735cad8f1556115d
David Sehi <david.sehi2016@gmail.com>
2023-06-06 11:11:27 +0000

    auth middleware initialization

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from "express";
+
+export async function verifyAccessToken(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // Assigned to Mariam
+  // TODO: Write the logic right here
+}
