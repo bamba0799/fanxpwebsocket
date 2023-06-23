99ce0fedce4097024592f17c0b1706f6143252e9
David <david.sehi2016@gmail.com>
2023-06-23 15:14:00 +0000

    Change route path

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,7 +3,7 @@ import * as authControllers from "../controllers/auth";
 
 const router = express.Router();
 
-router.post("/login-or-register", authControllers.signInOrSignUp);
+router.post("/", authControllers.getAuth);
 router.post("/verify-otp", authControllers.verifyOTP);
 router.post("/refresh-token", authControllers.refreshToken);
 
